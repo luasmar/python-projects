30d2ac65c5195c6d78d09047835e8468d704e989
Luan Asmar <80367132+luasmar@users.noreply.github.com>
2023-04-24 16:02:56 -0300

    Add files via upload

---
+++ password-generator.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+tamanho = int(input('Digite o tamanho da senha: '))
+
+caracteres = string.ascii_letters + string.digits + string.punctuation
+
+senha = ''
+for i in range(tamanho):
+    senha += random.choice(caracteres)
+
+print('A senha gerada é:', senha)
